b6fafbe61bfdf71186ba38d5dbb1e68f6d287005
kookxiang <kookxiang@gmail.com>
2017-04-19 22:02:31 +0800

    initial commit

---
+++ Chrome/background.js
@@ -0,0 +1,22 @@
+chrome.runtime.onMessageExternal.addListener((options, sender, callback) => {
+    try {
+        if (options.type === 'ping') callback(true);
+        const { site } = options;
+        if (!site.match(/^(http|https):/)) {
+            throw new Error('Illegal site url');
+        }
+        let filter = { url: site };
+        if (name in options) {
+            filter.name = options.name;
+        }
+        chrome.cookies.getAll(filter, cookies => {
+            callback({
+                succeed: true,
+                cookie: cookies.map(x => `${x.name}=${x.value}`).join('; ')
+            });
+        });
+        callback({ succeed: true, message: e.message });
+    } catch (e) {
+        callback({ succeed: false, message: e.message });
+    }
+});
---
+++ proxy.js
@@ -0,0 +1,54 @@
+(function () {
+    "use scrict";
+
+    var extensionId = 'pjoalcjgehonpmceajfmnkfpgjehnfak';
+    var isChrome = false;
+    var isAvailable = false;
+
+    if (!chrome || !chrome.runtime || !chrome.runtime.sendMessage) {
+        isChrome = false; // Not a chrome browser
+    } else {
+        isChrome = true;
+        chrome.runtime.sendMessage(extensionId, { action: 'ping' }, {}, function (result) {
+            console.log(result);
+            if (!result === true) {
+                isAvailable = true;
+            }
+            parent.postMessage({ type: 'cookie-proxy-loaded' }, '*');
+        });
+    }
+
+    function getDomain(url) {
+        var a = document.createElement('a');
+        a.href = url;
+        return a.hostname;
+    }
+
+    window.addEventListener('message', function (event) {
+        switch (event.data.action) {
+            case 'reload':
+                location.reload();
+                break;
+            case 'getStatus':
+                event.source.postMessage({
+                    id: event.data.id,
+                    succeed: true,
+                    isChrome: isChrome,
+                    isAvailable: isAvailable
+                }, '*');
+                break;
+            case 'getCookie':
+                var options = { site: getDomain(event.data.site) };
+                if (!options.site) {
+                    event.source.postMessage({ id: event.data.id, succeed: false, message: 'site url is not provided' });
+                }
+                if (!confirm('是否允许 ' + tldjs.getDomain(event.origin) + ' 获取你在网站 ' + options.site + ' 上的 Cookie？')) return;
+                chrome.runtime.sendMessage(extensionId, options, {}, function (result) {
+                    result.id = event.data.id
+                    event.source.postMessage(result, '*');
+                });
+                break;
+        }
+    }, false);
+})();
+
